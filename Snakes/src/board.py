1efb193e6321dd4bcaad0c507df1f7b35ca6d05c
Zlatko Lukic <zlatkolukic@outlook.com>
2020-12-24 10:52:23 +0100

    Added food drawing and positioning.

--- Snakes/src/board.py
+++ Snakes/src/board.py
@@ -1,11 +1,15 @@
 import random
 import winsound
 
+import pygame
 from PyQt5.QtCore import pyqtSignal, QBasicTimer, Qt, QRect
-from PyQt5.QtGui import QPainter, QImage
+from PyQt5.QtGui import QPainter, QImage, QColor
 from PyQt5.QtWidgets import QFrame
 
+
 from helpers import load_style_res, load_res
+
+import food
 
 
 class Board(QFrame):
@@ -18,10 +22,10 @@ class Board(QFrame):
         super(Board, self).__init__(parent)
         self.timer = QBasicTimer()
         self.snakes = []
-
-        self.food = []
-
         self.board = []
+
+        self.food=food.Food()
+        self.food.drop_food()
 
         self.setFocusPolicy(Qt.StrongFocus)
         self.setStyleSheet('border-image: url(' + load_style_res('grass.jpg') + ') 0 0 0 0 stretch center')
@@ -38,4 +42,18 @@ class Board(QFrame):
 
         self.timer.start(Board.SPEED, self)
 
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        rect = self.contentsRect()
+        boardtop = rect.bottom() - Board.HEIGHTINBLOCKS * self.square_height()
+
+        for pos in self.food.pos:
+            self.draw_food(painter, rect.left() + pos[0] * self.square_width(),
+                             boardtop + pos[1] * self.square_height())
+
+    def draw_food(self, painter, x, y):
+
+        image = QImage(load_res('apple.png'))
+
+        painter.drawImage(QRect(x + 1, y + 1, self.square_width() + 10, self.square_height() + 10), image)
 
--- Snakes/src/food.py
+++ Snakes/src/food.py
@@ -1,5 +1,38 @@
+import random
+
+import board
+
 
 class Food(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self):
+        self.x=random.randint(0, board.Board.WIDTHINBLOCKS - 1)
+        self.y=random.randint(0, board.Board.HEIGHTINBLOCKS - 1)
+        self.pos=[]
+
+    def drop_food(self):
+        i = random.randint(0, 1)
+        if i == 0:
+            j = random.randint(0, 1)
+            if j == 0:
+                self.x = random.randint(self.x, self.x + 3)
+            else:
+                self.x = random.randint(self.x - 3, self.x)
+        else:
+            j = random.randint(0, 1)
+            if j == 0:
+                self.y = random.randint(self.y, self.y + 3)
+            else:
+                self.y = random.randint(self.y - 3, self.y)
+
+        if self.x >= board.Board.WIDTHINBLOCKS - 1:
+            self.x -= 3
+        elif self.x <= 0:
+            self.x += 3
+        if self.y >= board.Board.HEIGHTINBLOCKS - 1:
+            self.y -= 3
+        elif self.y <= 0:
+            self.y += 3
+
+        self.pos.append([self.x, self.y])
+
+
